f8e3e581f816401597f883788a04b8644a6c7b1c
Sjoerd Nijland <sjoerd.nijland@outlook.com>
2019-04-17 14:23:13 +0200

    1.1.2
    http:/ to http:// in medium url

--- assets/js/build.js
+++ assets/js/build.js
@@ -15,7 +15,7 @@ class Build extends React.Component {
         return (
             <div className={ContainerClassName}>
                 <div className="one-half-fixed column _pl40">
-                    build: 1.1.1
+                    build: 1.1.2
                 </div>
                 <div className="one-half-fixed column right _pr40">
                     :)
--- assets/js/medium.js
+++ assets/js/medium.js
@@ -23,7 +23,7 @@ class Medium extends React.Component {
                         <p>Follow and join us and other Scrum practitioners on <i>Medium</i>.</p>
                         <p>over <span className="metric">111k</span> <span>minutes read /month</span></p>
                         <p>over <span className="metric">70k</span> <span>views /month</span></p>
-                        <p className="_mt20"><a href="https:/medium.com/serious-scrum" target="_blank" className="button">Tune in!</a></p>
+                        <p className="_mt20"><a href="https://medium.com/serious-scrum" target="_blank" className="button">Tune in!</a></p>
                     </div>
                 </div>
             </div>
