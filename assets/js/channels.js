f852ac760fc1838c9971b62552c58dbbf54dd86f
Sjoerd Nijland <sjoerd.nijland@outlook.com>
2019-04-17 17:29:43 +0200

    1.1.5
    tsunami icon

--- assets/js/channels.js
+++ assets/js/channels.js
@@ -11,11 +11,12 @@ class Channels extends React.Component {
     render() {
 
         var ContainerClassName = "channels row _pt20 _pb20 _pl40";
+        var tsunamiLogo = "images/tsunami-of-sense.svg";
 
         return (
             <div className={ContainerClassName}>
                 <div className="row _ml40">
-                    <h1>a tsunami of sense</h1>
+                    <h1>a tsunami of sense <img className='tsunami' src={tsunamiLogo}/> </h1>
                 </div>
                 <div className="row _ml40 _mr40">
                     <p>We are a professional community committed to <i>helping each other</i> in environments in which Scrum is not yet fully adopted and understood.</p>
