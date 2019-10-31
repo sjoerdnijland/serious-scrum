b67996cbb1c62bb29e1bab462804b14f10fb4245
Sjoerd Nijland <sjoerd.nijland@outlook.com>
2019-10-31 11:05:31 +0100

    1.3.14
    adding AGNC info

--- assets/js/build.js
+++ assets/js/build.js
@@ -15,7 +15,7 @@ class Build extends React.Component {
         return (
             <div className={ContainerClassName}>
                 <div className="one-half-fixed column _pl80">
-                    build: 1.3.13
+                    build: 1.3.14 - registered to AGNC - KVK: 58970037 - BTW: NL164404363B01- IBAN: NL84 RABO 0130 4761 53
                 </div>
                 <div className="one-half-fixed column right _pr40">
                     :)
