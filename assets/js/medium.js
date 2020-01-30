c3471f3bcc8f5fff8ca8cfb2d89e91b5e09338a9
Sjoerd Nijland <sjoerd.nijland@outlook.com>
2020-01-30 20:58:49 +0100

    1.3.15
    update metrics

--- assets/js/medium.js
+++ assets/js/medium.js
@@ -21,8 +21,8 @@ class Medium extends React.Component {
                     </div>
                     <div className="two-thirds column">
                         <p>Content by and for Scrum practitioners at <a href="https://medium.com/serious-scrum" target="_blank">Medium</a>.</p>
-                        <p><span className="metric">5.000</span> <span>followers</span></p>
-                        <p><span className="metric">100.000</span> <span>reads /month</span></p>
+                        <p><span className="metric">6.500</span> <span>followers</span></p>
+                        <p><span className="metric">200.000</span> <span>reads /month</span></p>
                         <p className="_pt20 buttonContainer"><a href="https://medium.com/serious-scrum" target="_blank" className="button">Tune in!</a></p>
                     </div>
                 </div>
--- assets/js/slack.js
+++ assets/js/slack.js
@@ -76,8 +76,8 @@ class Slack extends React.Component {
                     </div>
                     <div className="two-thirds column">
                         <p>Let’s have a serious chat at <a href="http://seriousscrum.com/invite" target="_blank">Slack</a> about Scrum. </p>
-                        <p><span className="metric">2.500</span> <span>members</span></p>
-                        <p><span className="metric">3.500</span> <span>messages / month</span></p>
+                        <p><span className="metric">2.750</span> <span>members</span></p>
+                        <p><span className="metric">5.500</span> <span>messages / month</span></p>
                         <p className="_pt20 buttonContainer">
                             <a className="button" id="requestInvite_button" href="http://seriousscrum.com/invite" target="_blank">Invite me!</a>
                         </p>
