383b4c1af9673f2cde33ff6169f27136b00e81c5
Sjoerd Nijland <sjoerd.nijland@outlook.com>
2020-06-18 14:19:13 +0200

    2.4.1

--- assets/js/tsunami.js
+++ assets/js/tsunami.js
@@ -19,7 +19,7 @@ class Tsunami extends React.Component {
                 <div className={ContainerClassName}>
                     <div className={bannerClassName}>
                         <h1>a Tsunami<br/>of Sense!</h1>
-                        <div>We are a global Scrum Community with over 10K Scrum Professionals</div>
+                        <div>We are an open global Scrum Community of 3.5K Scrum Professionals</div>
                         <p className="_pt20 _pl40 buttonContainer"><a className="button" id="requestInvite_button" href="http://seriousscrum.com/invite" target="_blank">Join us at Slack!</a></p>
 
                     </div>
