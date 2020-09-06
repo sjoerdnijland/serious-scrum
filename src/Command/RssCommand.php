2cd3d69ae8987cbb48c8575a52193a390e6c1c7c
Sjoerd Nijland <sjoerd.nijland@outlook.com>
2020-09-06 20:59:47 +0200

    2.9 rss feed command

---
+++ src/Command/RssCommand.php
@@ -0,0 +1,63 @@
+<?php
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use App\Manager\RssManager;
+
+class RssCommand extends Command
+{
+    // the name of the command (the part after "bin/console")
+    protected static $defaultName = 'app:rss';
+
+
+    public function __construct(RssManager $rssManager)
+    {
+        $this->rssManager = $rssManager;
+
+        parent::__construct();
+    }
+
+
+
+    protected function configure()
+    {
+        $this
+            // the short description shown while running "php bin/console list"
+            ->setDescription('Loads RSS articles.')
+
+            // the full command description shown when running the command with
+            // the "--help" option
+            ->setHelp('This command allows you to load articles from Medium serious scrum...')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // ... put here the code to run in your command
+        // outputs multiple lines to the console (adding "\n" at the end of each line)
+        $output->writeln([
+            'Reload',
+            '============',
+            '',
+        ]);
+
+        $rss = $this->rssManager->getRSS('https://medium.com/feed/serious-scrum');
+
+        print($rss);
+
+        return 1;
+
+        // this method must return an integer number with the "exit status code"
+        // of the command. You can also use these constants to make code more readable
+
+        // return this if there was no problem running the command
+        // (it's equivalent to returning int(0))
+        //return Command::SUCCESS;
+
+        // or return this if some error happened during the execution
+        // (it's equivalent to returning int(1))
+        // return Command::FAILURE;
+    }
+}
---
+++ src/Manager/RssManager.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nijland
+ * Date: 06/09/2020
+ * Time: 12:09
+ */
+
+namespace App\Manager;
+
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use App\Entity\Article;
+use App\Entity\Category;
+use Doctrine\ORM\EntityManagerInterface;
+
+class RssManager
+{
+
+    /**
+     * @var ClientInterface
+     */
+    private $client;
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+
+    public function __construct(HttpClientInterface $client, EntityManagerInterface $entityManager)
+    {
+        $this->client = $client;
+        $this->em = $entityManager;
+
+    }
+
+    public function getRSS($url)
+    {
+
+        $em = $this->em;
+
+        $xml = simplexml_load_file($url,null
+            , LIBXML_NOCDATA);
+
+        //print_r($xml->channel);
+
+        foreach($xml->channel->item as $item){
+            $title = $item->title;
+            $url = explode("?source",$item->link)[0];
+
+            $parseImage = explode("src=\"", $item->description);
+            if(isset($parseImage[1])){
+                $parseImage = explode("\"", $parseImage[1]);
+                $thumbnail = $parseImage[0];
+            }else{
+                $thumbnail = "";
+            }
+
+            $parseIntro = explode("medium-feed-snippet\">", $item->description);
+            if(isset($parseIntro[1])){
+                $parseIntro = explode("</p>", $parseIntro[1]);
+                $intro = $parseIntro[0];
+            }else{
+                $intro = "";
+            }
+
+            $children = $item->children('http://purl.org/dc/elements/1.1/');
+            $author = $children->creator;
+            $author = $author[0];
+
+            $article = new Article();
+
+            $article->setUrl($url);
+            $article->setTitle($title);
+            $article->setThumbnail($thumbnail);
+            $article->setIntro($intro);
+            $article->setAuthor($author);
+
+            $category = $em->getRepository(Category::class)
+                ->findOneBy([
+                    'id' => 1,
+                ]);
+
+            $article->setCategory($category);
+
+            $urlAlreadyExists = $em->getRepository(Article::class)
+                ->findArticleByUrl($url);
+
+            if($urlAlreadyExists){
+                continue;
+            }
+
+            $em->persist($article);
+
+            $em->flush();
+
+        }
+
+        return 1;
+    }
+}
