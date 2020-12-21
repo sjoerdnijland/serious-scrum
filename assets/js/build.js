5f560bb6af62cbc4f137ec5effbf42f2771deb5d
Sjoerd Nijland <sjoerd.nijland@outlook.com>
2020-12-21 22:44:20 +0100

    2.22.3.1

--- assets/js/build.js
+++ assets/js/build.js
@@ -16,7 +16,7 @@ class Build extends React.Component {
         return (
             <div className={ContainerClassName}>
                 <div className="two-thirds column ">
-                    increment: 2.21.2 - AGNC - Serious Scrum has a Registered Trademark - KVK: 58970037 - VAT/BTW: NL164404363B01- IBAN: NL84 RABO 0130 4761 53
+                    increment: 2.22.3 - AGNC - Serious Scrum has a Registered Trademark - KVK: 58970037 - VAT/BTW: NL164404363B01- IBAN: NL84 RABO 0130 4761 53
                 </div>
                 <div className="one-third column right _pr40">
                     :)
