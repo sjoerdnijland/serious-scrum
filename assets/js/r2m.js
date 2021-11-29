fa6d2b5838cb3083b6c6b9f22cd0241dfad42078
Sjoerd Nijland <sjoerd.nijland@outlook.com>
2021-11-29 21:07:45 +0100

    3.18 scrum values chapter

--- assets/js/r2m.js
+++ assets/js/r2m.js
@@ -75,6 +75,7 @@ import '../js/r2m_format';
 import '../js/r2m_testimonials';
 import '../js/r2m_testimonial';
 import '../js/r2m_theory';
+import '../js/r2m_scrumvalues';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -677,6 +678,7 @@ class R2M extends React.Component {
                 <R2MAgile label={this.state.label} module={this.state.module}/>
                 <R2MDefinition label={this.state.label} module={this.state.module}/>
                 <R2MTheory label={this.state.label} module={this.state.module}/>
+                <R2MScrumValues label={this.state.label} module={this.state.module}/>
                 <R2MCoaching label={this.state.label} module={this.state.module}/>
                 <R2MSelfmanagement label={this.state.label} module={this.state.module}/>
 
---
+++ assets/js/r2m_scrumvalues.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+class R2MScrumValues extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+
+    render() {
+
+
+        let ContainerClassName = "r2m_scrumvalues row";
+
+        if(this.props.label != 'scrum-values){
+            ContainerClassName += " hidden";
+        }
+
+        const bannerClassName = "homeBanner";
+
+        return (
+
+                <div className={ContainerClassName}>
+                    <div className={bannerClassName}>
+                        <div>Road to Mastery</div>
+                        <h1>Scrum Values</h1>
+                        <div>
+                            "Trust is a bit like the air we breathe, it sustains, it brings things to life, but without it... we are left gasping" -John Albrecht<br/>
+                        </div>
+
+                    </div>
+                </div>
+
+
+        );
+    }
+}
+window.R2MScrumValues = R2MScrumValues;
