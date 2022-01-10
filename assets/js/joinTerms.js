9cc4f5b48ab3a0004a0fa4a500471d7c55818fd7
Sjoerd Nijland <sjoerd.nijland@outlook.com>
2022-01-10 09:27:57 +0100

    2.28

--- assets/js/joinTerms.js
+++ assets/js/joinTerms.js
@@ -19,7 +19,7 @@ class JoinTerms extends React.Component {
 
         return (
             <div className={containerClassName}>
-                <br/><span className="subscription">Subscription is €175,- p/m, starting from the first training.</span><br/>
+                <br/><span className="subscription">Subscription is €125,- p/m, starting from the first training.</span><br/>
                 <input type="checkbox" className={"_mr10"} checked={this.props.terms} onChange={this.toggleCheckBox}  />
                 I agree to the <a href="/page/subscription-terms-and-conditions" target="_blank">terms and conditions</a>.<br/>
 
