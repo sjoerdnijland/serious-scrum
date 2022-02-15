c5aa6fefae6d1c9a3adc887842adf10a1603c1db
Sjoerd Nijland <sjoerd.nijland@outlook.com>
2022-02-15 21:11:37 +0100

    3.43.4

--- assets/js/r2m_home.js
+++ assets/js/r2m_home.js
@@ -32,14 +32,15 @@ class R2MHome extends React.Component {
                     <div className={bannerClassName}>
                         <h1>Road to Mastery</h1>
                         <div className="title">
-                            Master Scrum together with a playbook including 500+ co-active plays.
+                            Master Scrum together.
                             <ul>
                                 <li>Weekly recurring virtual 2-hour training;</li>
                                 <li>A 6-month journey off the beaten track;  </li>
                                 <li>Share experiences and explore new ways;</li>
+                                <li>Expand your playbook with over 500 creative plays;</li>
                             </ul>
                         </div>
-                        <p className="_pt20 _pl40 buttonContainer joinButton _fl"><a className="button" id="requestInvite_button" href="/page/the-road-to-mastery-r2m">About the journey</a></p>
+                        <p className="_pt40 _pl40 buttonContainer joinButton _fl"><a className="button" id="requestInvite_button" href="/page/the-road-to-mastery-r2m">About the journey</a></p>
                         <div className="mirozoom">
                             <div>
                                 <img src={'/images/zoom.png'}/>
