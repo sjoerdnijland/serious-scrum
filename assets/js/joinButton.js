e1dec09d35e1ff06b7a347e461e2b9b62ce32dfc
Sjoerd Nijland <sjoerd.nijland@outlook.com>
2022-02-15 22:29:46 +0100

    3.43.12

--- assets/js/joinButton.js
+++ assets/js/joinButton.js
@@ -7,7 +7,7 @@ class JoinButton extends React.Component {
     }
 
     handleClick(){
-        this.props.functions.setR2MMenu('join');
+        this.props.functions.setR2MMenu('r2m/join');
         this.props.functions.joinTravelgroup(this.props.travelgroup);
     }
 
