84636101ed46672d800330a076f6f582954f0197
Sjoerd Nijland <sjoerd.nijland@outlook.com>
2022-08-04 16:28:34 +0200

    3.81.2

--- assets/js/travelgroupSelect.js
+++ assets/js/travelgroupSelect.js
@@ -56,7 +56,7 @@ class TravelgroupSelect extends React.Component {
 
                     options.push(option);
                 }
-                if(!travelgroups[i].isFuture || travelgroups[i].isSoldOut || !travelgroups[i].isActive || travelgroups[1].host == 'MaibornWolff'){
+                if(!travelgroups[i].isFuture || travelgroups[i].isSoldOut || !travelgroups[i].isActive || travelgroups[i].host == 'MaibornWolff'){
                     continue;
                 }
                 let option= [];
