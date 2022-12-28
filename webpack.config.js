6ddebd3454b6f01fe4a1828e1feefbeb37f526eb
Sjoerd Nijland <sjoerd.nijland@outlook.com>
2022-12-28 19:39:24 +0100

    4.1.28

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ Encore
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
      // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
-    //.enableStimulusBridge('./assets/controllers.json')
+    .enableStimulusBridge('./assets/controllers.json')
 
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
