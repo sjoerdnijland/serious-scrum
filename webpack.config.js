136b1bb86dffcbfa86687b747c59fae1f7ad9782
Sjoerd Nijland <sjoerd.nijland@outlook.com>
2022-12-28 02:05:08 +0100

    4.1.9

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
 
-    .addEntry('app', './assets/js/app.js') 
+    .addEntry('app', './assets/js/app.js')
     .addEntry('page', './assets/js/page.js')
     .addEntry('r2m_page', './assets/js/r2m_page.js')
     .addEntry('patreon', './assets/js/patreon.js')
