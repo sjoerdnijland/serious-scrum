b7ef314e010fc0653c36051c7f6bd611afbcfba8
Sjoerd Nijland <sjoerd.nijland@outlook.com>
2023-01-25 11:01:34 +0100

    4.3

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
