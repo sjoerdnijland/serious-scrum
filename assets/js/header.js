a4fe09545adef1fd344194707a0b245e742cd454
Sjoerd Nijland <sjoerd.nijland@outlook.com>
2023-08-29 10:06:58 +0200

    4.4.1

--- assets/js/header.js
+++ assets/js/header.js
@@ -24,15 +24,12 @@ class Header extends React.Component {
                 </a>
                 <div className="headerOptions _pr10 _pt20">
                      <CategoryMenu functions={functions} expanded={this.props.expanded} site="serious"/>
-                    <div className={'headerMenuItem'}>
-                        <a href={'https://join.slack.com/t/serious-scrum/shared_invite/zt-tpyfeaty-~lkfHitvhGtn9fo~1HnwMQ'} target={'_blank'}>Sign In</a>
-                    </div>
                     <Search functions={functions} value={this.props.search} type="desktop"/>
                     <SocialMenu type="header"/>
 
                     <PublishButton functions={functions} user={this.props.user}/>
                     <div className={'headerMenuItem'}>
-                        <a href={'/r2m'} target={'_blank'}>Road to Mastery</a>
+                        <a href={'https://www.road2mastery.com'} target={'_blank'}>Road to Mastery</a>
                     </div>
 
                 </div>
